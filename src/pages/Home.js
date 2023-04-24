d35316d75f24082d3976758a8853a26861359e89
amitp138 <amitpande1008@gmail.com>
2023-04-24 16:12:29 +0530

    Modify the ui according to Screen

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,52 @@
-import React from 'react'
-import Sidebar from '../components/Sidebar'
-import Chat from "../components/Chat"
-const Home = () => {
+import React, { useEffect, useState } from "react";
+import Sidebar from "../components/Sidebar";
+import Chat from "../components/Chat";
+const LaptopView = () => {
+  // Render two components together
   return (
     <div className="home">
-        <div className="container">
-            <Sidebar/>
-            <Chat/>
-        </div>
+      <div className="container">
+        <Sidebar />
+        <Chat />
+      </div>
     </div>
-  )
-}
+  );
+};
+const MobileView = () => {
+  const [showSidebar, setShowSidebar] = useState(true);
 
-export default Home
+  const handleButtonClick = () => {
+    // Toggle the display of Component1 and Component2
+    setShowSidebar(!showSidebar);
+  };
+
+  // Render only one component at a time based on state
+  return (
+    <div className="home">
+      <div className="container">
+        {showSidebar ? <Sidebar /> : <Chat />}
+        <button onClick={handleButtonClick}>👈</button>
+      </div>
+    </div>
+  );
+};
+
+const Home = () => {
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 768);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  return <> {isMobile ? <MobileView /> : <LaptopView />}</>;
+};
+
+export default Home;
